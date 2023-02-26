fe5dd2c97ce9bc9fd875db41cf3cdd7c0bd2f9dc
henriits <121551949+henriits@users.noreply.github.com>
2023-02-26 14:03:27 +0200

    get response method

--- backend.py
+++ backend.py
@@ -1,2 +1,23 @@
+import openai
+import os
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=4000,
+            temperature=0.5,     # sets how accurate the answer will be, from 0 to 1
+        ).choices[0].text
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("explain python simply")
+    print(response)
+
